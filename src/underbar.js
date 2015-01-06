0dc27dadb667bfd267ba098b35c87b14dc9b378b
Pavan Ravipati <poppin3000@gmail.com>
2015-01-05 16:16:31 -0800

    Implemented every function. Passes all tests

--- src/underbar.js
+++ src/underbar.js
@@ -210,6 +210,20 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+      if (iterator === undefined) {
+	  var iterator = _.identity;
+      }
+      return _.reduce(collection, function(allTrue, item) {
+	  if (!iterator(item)) {
+	      return false;
+	  }
+	  else if (!allTrue) {
+	      return false;
+	  }
+	  else {
+	      return true;
+	  }
+      }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
